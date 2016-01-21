02f07b6baaf4d238a28968d2125baf9e44136fc4
Oky Sabeni <oky.sabeni@gmail.com>
2016-01-21 15:54:31 -0800

    first commit

---
+++ CallbackExample/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  CallbackExample
+//
+//  Created by Oky Sabeni on 1/21/16.
+//  Copyright © 2016 Oky Sabeni. All rights reserved.
+//
+
+import Foundation
--- CallbackExample/ViewController.swift
+++ CallbackExample/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Alamofire
 
+class Person: NSObject {
+    static let sharedInstance = Person()
+    var object: AnyObject
+    private override init() {
+        object = ""
+    }
+}
+
+class ViewController: UIViewController {
+    var object = Person.sharedInstance.object
+    var anotherObject: AnyObject?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(unsafeAddressOf(object)) // 0x00007f91835899b0
+        print(unsafeAddressOf(Person.sharedInstance.object)) // 0x00007f91835899b0
+        Person.sharedInstance.object = "SOMETHING"
+        print(self.object)
+        get { _ in
+            // Doesn't work
+            Person.sharedInstance.object = "SOMETHING"
+            print(self.object)
+            print(self.anotherObject)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func get(completionHandler: (result: AnyObject?) -> Void) {
+        Alamofire.request(.GET, "http://private-9bd21-callbackexample1.apiary-mock.com/questions")
+            .responseJSON { [unowned self] response in
+                print(unsafeAddressOf(self.object)) // 0x00007f91835899b0
+                print(unsafeAddressOf(Person.sharedInstance.object)) // 0x00007f91835899b0
+                Person.sharedInstance.object = response.result.value!
+                self.anotherObject = response.result.value!
+                completionHandler(result: response.result.value)
+        }
     }
-
-
 }
 
